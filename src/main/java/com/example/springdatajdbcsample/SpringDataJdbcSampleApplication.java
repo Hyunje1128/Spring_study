6c08eedcfd4b2ec63c7f116896adb3fa9a379c41
hyunje1128 <guswp1128@gmail.com>
2023-06-06 02:04:57 +0900

    SpringDataJdbcSampleApplication

--- src/main/java/com/example/springdatajdbcsample/SpringDataJdbcSampleApplication.java
+++ src/main/java/com/example/springdatajdbcsample/SpringDataJdbcSampleApplication.java
@@ -1,13 +1,59 @@
 package com.example.springdatajdbcsample;
 
+import com.example.springdatajdbcsample.entity.Member;
+import com.example.springdatajdbcsample.repository.MemberCrudRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.SocketOption;
+
+/**
+ * 스프링 부트 기동 클래스
+ */
 @SpringBootApplication
-public class SpringDataJdbcSampleApplication {
+public class SpringDataJdbcSampleApplication /** 실제 작업을 처리하는 클래스 */{
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringDataJdbcSampleApplication.class, args);
+        SpringApplication.run(SpringDataJdbcSampleApplication.class, args)
+            .getBean(SpringDataJdbcSampleApplication.class).execute(); /** execute 메서드를 호출 */
     }
 
+    @Autowired
+    MemberCrudRepository repository;
+
+    /**
+     *  등록과 전체 취득 처리
+     */
+    private void execute()
+    /** execute 메서드로 등록 메서드와 전체 취득 메서드를 호출하도록 작성 */{
+        // 등록
+        executeInsert();
+        // 전체 취득
+        executeSelect();
+    }
+
+    /**
+     *  등록
+     */
+    private void executeInsert() {
+        // 엔티티 생성 (id는 자동 부여되기 때문에 null을 설정)
+        Member member = new Member(null, "이순신");
+        // 리포지토리를 이용해 등록을 수행하고 결과를 취득
+        member = repository.save(member);
+        // 결과를 표시
+        System.out.println("등록 데이터:" + member);
+    }
+
+    /**
+     *  전체 취득
+     */
+    private void executeSelect() {
+        System.out.println("--- 전체 데이터를 취득합니다. ---");
+        // 리포지토리를 이용해 전체 데이터를 취득
+        Iterable<Member> members = repository.findAll();
+        for (Member member : members) {
+            System.out.println(member);
+        }
+    }
 }
